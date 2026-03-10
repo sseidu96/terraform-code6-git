f761a4239b612c07bc85311dab267a370b9cce7c
sseidu96 <safiatus65@gmail.com>
2026-03-10 02:09:22 -0400

    ticket 3

---
+++ file1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Author : Safiatu Seidu
+#Modified By : Safiatu Seidu
+#Date : MARCH 10 2026
+# ----linux commands---------------------------------------------------------------------------------------------##
+uname -r
+lscpu
+nproc
+pwd
+whoami
+cat /etc/os-release
+date
+cal
+free -m
+top
+lsof grep | 8000: python3 
+mkdir manager
+man 
+tail -3 /etc/passwd
+cat /etc/group
